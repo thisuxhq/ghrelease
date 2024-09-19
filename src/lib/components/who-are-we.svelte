d5f9aff5a8ee3706cc090acce86587aa7899c099
SpikeySanju <spikeysanju98@gmail.com>
2024-09-19 21:38:17 +0530

    Remove unnecessary margin from text elements in 'Who Are We' component for cleaner layout

--- src/lib/components/who-are-we.svelte
+++ src/lib/components/who-are-we.svelte
@@ -11,9 +11,9 @@
 		<Popover.Content class="w-80 p-4 ">
 			<div class="space-y-4">
 				<div class="flex flex-col items-center justify-between gap-3 rounded-lg bg-white">
-					<p class="ml-2">You’re curious, right? Check out what else we’ve created just for you!</p>
+					<p>You’re curious, right? Check out what else we’ve created just for you!</p>
 
-					<div class="flex w-full flex-row gap-3 px-3">
+					<div class="flex w-full flex-row gap-3">
 						<a href="https://uiino.com?utm_source=thisux&utm_medium=link" target="_blank" class="underline text-blue-500 hover:text-blue-700" aria-label="Uiino">
 							Uiino
 						</a>
@@ -46,9 +46,9 @@
 						</a>
 					</div>
 
-					<p class="ml-2">Got a cool project idea? We can help.</p>
+					<p >Got a cool project idea? We can help.</p>
 
-					<div class="flex w-full flex-row items-start justify-start gap-3 px-3">
+					<div class="flex w-full flex-row items-start justify-start gap-3">
 						<a href="https://ghrelease.thisux.com?utm_source=thisux&utm_medium=button&utm_campaign=visit_us" target="_blank" class="inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm px-3 py-2" data-umami-event="Visit Us Button">Visit us</a>
 						<a href="https://cal.com/imsanju/15min?utm_source=thisux&utm_medium=button" target="_blank" class="inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 shadow py-2 px-3" data-umami-event="Lets Talk Button">Let’s talk</a>
 					</div>
